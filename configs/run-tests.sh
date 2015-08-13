b0ff636b87162e73ff8c334614fce344b310f95f
Blake Blackshear <bblackshear@nextgxdx.com>
2015-08-13 15:19:44 -0500

    cleanup output

--- configs/run-tests.sh
+++ configs/run-tests.sh
@@ -5,17 +5,17 @@ sleep 2
 
 echo -e "\033[32m ===> Testing OrmLite\033[0m"
 ab -n 200 -c 100 http://127.0.0.1:9832/ormlite
-avg_time=$(curl 127.0.0.1:9832/times\?format=json)
+avg_time=$(curl -s 127.0.0.1:9832/times\?format=json)
 echo -e "\033[32m ===> OrmLite $avg_time ms\033[0m"
 
 echo -e "\033[32m ===> Testing Nhibernate\033[0m"
 ab -n 200 -c 100 http://127.0.0.1:9832/nhibernate
-avg_time=$(curl 127.0.0.1:9832/times\?format=json)
+avg_time=$(curl -s 127.0.0.1:9832/times\?format=json)
 echo -e "\033[32m ===> Nhibernate $avg_time ms\033[0m"
 
 echo -e "\033[32m ===> Testing Npgsql\033[0m"
 ab -n 200 -c 100 http://127.0.0.1:9832/npgsql
-avg_time=$(curl 127.0.0.1:9832/times\?format=json)
+avg_time=$(curl -s 127.0.0.1:9832/times\?format=json)
 echo -e "\033[32m ===> Npgsql $avg_time ms\033[0m"
 
 kill $mono_pid
@@ -27,17 +27,17 @@ sleep 2
 
 echo -e "\033[32m ===> Testing OrmLite\033[0m"
 ab -n 200 -c 100 http://127.0.0.1:9832/ormlite
-avg_time=$(curl 127.0.0.1:9832/times\?format=json)
+avg_time=$(curl -s 127.0.0.1:9832/times\?format=json)
 echo -e "\033[32m ===> OrmLite $avg_time ms\033[0m"
 
 echo -e "\033[32m ===> Testing Nhibernate\033[0m"
 ab -n 200 -c 100 http://127.0.0.1:9832/nhibernate
-avg_time=$(curl 127.0.0.1:9832/times\?format=json)
+avg_time=$(curl -s 127.0.0.1:9832/times\?format=json)
 echo -e "\033[32m ===> Nhibernate $avg_time ms\033[0m"
 
 echo -e "\033[32m ===> Testing Npgsql\033[0m"
 ab -n 200 -c 100 http://127.0.0.1:9832/npgsql
-avg_time=$(curl 127.0.0.1:9832/times\?format=json)
+avg_time=$(curl -s 127.0.0.1:9832/times\?format=json)
 echo -e "\033[32m ===> Npgsql $avg_time ms\033[0m"
 
 kill $mono_pid
